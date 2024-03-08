a96cd9e0537646715aef61d74f97540eaa61e7f1
altship <1376552345@qq.com>
2024-03-08 15:31:33 +0800

    Dictionary traverse and reverse completed

--- Lab01.py
+++ Lab01.py
@@ -16,7 +16,7 @@ def BinarySearch(nums: List[int], target: int) -> int:
 def MatrixAdd(A: List[List[int]], B: List[List[int]]) -> List[List[int]]:
     n = len(A)
     m = len(A[0])
-    ans = [[0] * m for _ in range(n)]
+    ans = [[0] * m for _ in range(n)]  # Create a new list to store result to avoid change origin data.
     for i in range(n):
         for j in range(m):
             ans[i][j] = A[i][j] + B[i][j]
@@ -33,6 +33,14 @@ def MatrixMul(A: List[List[int]], B: List[List[int]]) -> List[List[int]]:
             ans[i][j] = sum(A[i][x] * B[x][j] for x in range(m))
     return ans
 
+
+def ReverseKeyValue(dict1: dict) -> dict:
+    redict = {}
+    for key, value in dict1.items():
+        redict[value] = key
+    return redict
+
+
 if __name__ == '__main__':
     a = [0, 1, 2, 3, 4, 5, 6]
     print(BinarySearch(a, -3))
@@ -40,6 +48,8 @@ if __name__ == '__main__':
     B = [[5, 6, 7, 8], [5, 6, 7, 8], [5, 6, 7, 8], [5, 6, 7, 8]]
     C = MatrixAdd(A, B)
     D = MatrixMul(A, B)
+    dict1 = {'Alice': '001', 'Bob': '002', 'Calvin': '003'}
+    dict2 = ReverseKeyValue(dict1)
     print(f"{len(A)}, {len(A[0])}")
     for i in C:
         for j in i:
@@ -50,3 +60,4 @@ if __name__ == '__main__':
         for j in i:
             print(f"{j} ", end="")
         print()
+    print(dict2)
